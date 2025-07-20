ef545195ee4d5e6a5b90237615b3ec4e51d219ca
McMuffin12 <bradysnow1@gmail.com>
2025-07-20 02:52:18 +0000

    Fixed deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio.bradylabs.net',
   vite: {
-    base: "/portfolio-site/",
     plugins: [tailwindcss()],
   },
 });
